325ae6a904c00554a4630bd5ba57114f4ef44647
CodeMinion <hernandez.frank@gmail.com>
2011-08-01 21:00:45 -0400

    First Version

---
+++ Aggregation_staruml-java/staruml-java/src/code/JavaAddIn_TLB.pas
@@ -0,0 +1,54 @@
+unit JavaAddIn_TLB;
+
+// ************************************************************************ //
+// WARNING                                                                    
+// -------                                                                    
+// The types declared in this file were generated from data read from a       
+// Type Library. If this type library is explicitly or indirectly (via        
+// another type library referring to this type library) re-imported, or the   
+// 'Refresh' command of the Type Library Editor activated while editing the   
+// Type Library, the contents of this file will be regenerated and all        
+// manual modifications will be lost.                                         
+// ************************************************************************ //
+
+// PASTLWTR : 1.2
+// File generated on 7/31/2011 8:31:17 PM from Type Library described below.
+
+// ************************************************************************  //
+// Type Lib: D:\Documents and Settings\MaxDraco\Desktop\PhD\Software Engineering\Tools\staruml-src\staruml-java\src\code\JavaAddIn.tlb (1)
+// LIBID: {90567572-0C0C-49B4-84FC-6BBD8EA8CE57}
+// LCID: 0
+// Helpfile: 
+// HelpString: JavaAddIn Library
+// DepndLst: 
+//   (1) v2.0 stdole, (D:\WINDOWS\system32\stdole2.tlb)
+// ************************************************************************ //
+{$TYPEDADDRESS OFF} // Unit must be compiled without type-checked pointers. 
+{$WARN SYMBOL_PLATFORM OFF}
+{$WRITEABLECONST ON}
+{$VARPROPSETTER ON}
+interface
+
+uses Windows, ActiveX, Classes, Graphics, StdVCL, Variants;
+  
+
+// *********************************************************************//
+// GUIDS declared in the TypeLibrary. Following prefixes are used:        
+//   Type Libraries     : LIBID_xxxx                                      
+//   CoClasses          : CLASS_xxxx                                      
+//   DISPInterfaces     : DIID_xxxx                                       
+//   Non-DISP interfaces: IID_xxxx                                        
+// *********************************************************************//
+const
+  // TypeLibrary Major and minor versions
+  JavaAddInMajorVersion = 1;
+  JavaAddInMinorVersion = 0;
+
+  LIBID_JavaAddIn: TGUID = '{90567572-0C0C-49B4-84FC-6BBD8EA8CE57}';
+
+
+implementation
+
+uses ComObj;
+
+end.
